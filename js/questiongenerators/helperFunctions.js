96b55c1634680e7384feba7b1ae48936de2436c9
TonyMorinello <MorinelloA@gmail.com>
2016-12-16 14:03:29 -0500

    nameGenerator function improved

--- js/questiongenerators/helperFunctions.js
+++ js/questiongenerators/helperFunctions.js
@@ -110,84 +110,11 @@ function GCF(a,b) {
 function generateName(){
     var temp = Math.floor(Math.random() * 25);
     var name;
-    switch(temp) {
-        case 0:
-            name = 'Megan';
-            break;
-        case 1:
-            name = 'Andy';
-            break;
-        case 2:
-            name = 'Dave';
-            break;
-        case 3:
-            name = 'Tristyn';
-            break;
-        case 4:
-            name = 'Wendy';
-            break;
-        case 5:
-            name = 'James';
-            break;
-        case 6:
-            name = 'Lydia';
-            break;
-        case 7:
-            name = 'Vanessa';
-            break;
-        case 8:
-            name = 'Mike';
-            break;
-        case 9:
-            name = 'Reggie';
-            break;
-        case 10:
-            name = 'Mark';
-            break;
-        case 11:
-            name = 'Bethany';
-            break;
-        case 12:
-            name = 'Yolanda';
-            break;
-        case 13:
-            name = 'Carmen';
-            break;
-        case 14:
-            name = 'Carlos';
-            break;
-        case 15:
-            name = 'Frank';
-            break;    
-        case 16:
-            name = 'Rachel';
-            break;
-        case 17:
-            name = 'Tonya';
-            break;
-        case 18:
-            name = 'Jennifer';
-            break;
-        case 19:
-            name = 'Brian';
-            break;
-        case 20:
-            name = 'Paula';
-            break;
-        case 21:
-            name = 'Hillary';
-            break;
-        case 22:
-            name = 'Donald';
-            break;
-        case 23:
-            name = 'Joe';
-            break;
-        case 24:
-            name = 'Bill';
-            break;                                                  
-        default:
-            name = 'Tony';
-    }
-    return name;
+    var names = ['Andy', 'Anthony', 'Becky', 'Bethany', 'Bill', 'Brian', 'Carlos', 
+    'Carmen', 'Dave', 'Donald', 'Ethan', 'Frank', 'Francis', 'Greg', 'Hillary', 'Ike', 
+    'Isaiah', 'James', 'Jennifer', 'Joe', 'Kelly', 'Kathy', 'Lydia', 'Mark', 'Megan', 
+    'Mike', 'Nick', 'Olivia', 'Oliver', 'Paula', 'Quinn', 'Rachel', 'Reggie', 'Sam', 'Tony', 
+    'Tonya', 'Tristyn', 'Vanessa', 'Vicky', 'Wendy', 'Yolanda'];
+
+    return names[Math.floor(Math.random() * names.length)];
 }
